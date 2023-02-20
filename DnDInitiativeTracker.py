19e2f78e787f604aed24548149825092aeccbc27
vbaws <125846837+vbaws@users.noreply.github.com>
2023-02-20 23:28:44 +0000

    Update DnDInitiativeTracker.py
    Add support for on the fly editing of initiative values. Allows a DM to cope with the Delay option

--- DnDInitiativeTracker.py
+++ DnDInitiativeTracker.py
@@ -30,6 +30,22 @@ def AddToInit(Data):
     else:
         return Data
 
+def EditInit(Data, CurrTurn):
+    CurrPlayer = Data['Name'][CurrTurn]
+    inp = input('Change initiative of which ID: ')
+    if inp.isnumeric():
+        ID = int(inp)
+        if ID < len(Data['Name']):
+            name =  Data['Name'][ID]
+            print('You selected ', name, ', what do you want to change their Initiative to?', sep='')
+            inp2 = input('New Initiative: ')
+            if inp2.isnumeric():
+                Data['Init'][ID] = int(inp2)
+                Data = InitSort(Data)
+                CurrTurn = Data['Name'].loc[lambda x: x==name].index.tolist()[0]
+                print(Data)
+    return Data, CurrTurn
+
 def ApplyDamage(Data):
     inp = input('Who do you wish to apply damage to? (Insert ID from the tracker list, invalid choice will result in return to action selection) ')
     if inp.isnumeric():
@@ -108,7 +124,6 @@ if __name__ == '__main__':
     elif len(sys.argv) == 2:
         if os.path.exists(sys.argv[1]):
             Data = pd.read_csv(sys.argv[1], sep=',', dtype=str)
-            
             for ind in Data.index:
                 Data.at[ind,'Mod'] = int(Data.at[ind,'Mod'])
                 Data.at[ind,'HP'] = int(Data.at[ind,'HP'])
@@ -130,14 +145,17 @@ if __name__ == '__main__':
         #Print initiatives
         print(Data)
         #Ask for actions
-        Instruction = input ('\nAvailable actions:\n\t[aA] Add - Add someone to the list;\n\t[rR] Remove - remove someone from the list;\n\t[dD] Damage - Apply damage to someone;\n\t[cC] Condition - Apply a condition to someone;\n\t[nN] Next - Go to next turn;\n\t[tT] Turn - Set a specific turn;\n\t[sS] Save - Save an initiative file;\n\t[qQ] Quit - Exit from the tracker.\nChoose an action ')
-        if Instruction.lower() in ('a', 'd', 'c', 'n', 'q', 'r', 's', 't'):
+        Instruction = input ('\nAvailable actions:\n\t[aA] Add - Add someone to the list;\n\t[rR] Remove - remove someone from the list;\n\t[eE] Edit - Change someones initiative value;\n\t[dD] Damage - Apply damage to someone;\n\t[cC] Condition - Apply a condition to someone;\n\t[nN] Next - Go to next turn;\n\t[tT] Turn - Set a specific turn;\n\t[sS] Save - Save an initiative file;\n\t[qQ] Quit - Exit from the tracker.\nChoose an action ')
+        if Instruction.lower() in ('a', 'd', 'c', 'e', 'n', 'q', 'r', 's', 't'):
             #Add to initiative
             if Instruction.lower() == 'a':
                 Data = AddToInit(Data)
             #Remove from list
             elif Instruction.lower() == 'r':
                 Data, CurrTurn = RemoveFromInit(Data, CurrTurn)
+            #Edit Initiative from list
+            elif Instruction.lower() == 'e':
+                Data, CurrTurn = EditInit(Data, CurrTurn)
             #Apply damage
             elif Instruction.lower() == 'd':
                 Data = ApplyDamage(Data)
